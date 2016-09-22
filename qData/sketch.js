024991fb1d9ed65fd56cfee680dd55e5a2441775
Riley Hoonan <hoonr188@newschool.edu>
2016-09-22 03:25:58 +0000

    updating colors

--- qData/sketch.js
+++ qData/sketch.js
@@ -2,10 +2,13 @@ var startYear = 1976;
 var endYear = 2015;
 var africanCountries = ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde", "Cameroon", "Central African Republic (CAR)", "Chad", "Comoros", "Democratic Republic of the Congo", "Republic of the Congo", "Cote d'Ivoire", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone", "Somalia", "South Africa", "South Sudan", "Sudan", "Swaziland", "Tanzania", "Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"];
 
-var chartWidth = 100;
-var allChartWidth = chartWidth * africanCountries.length;
+var chartWidth = 150;
+var barWidth = 80;
+var allChartWidth = chartWidth * africanCountries.length +50;
 
 var leftPad = 50;
+
+
 
 function setup() {
     
@@ -13,16 +16,24 @@ function setup() {
     //loadTable("worldbank_GINIbyCountry_AfricaOnly.csv", "csv", "header", showWBData);
     loadTable("wiid.csv", "csv", "header", showWIIDData);
     loadTable("pts.csv", "csv", "header", showPTSData);
+    
 
 }
 
 function labels(color) {
     
     for (var c=0; c<africanCountries.length; c++) {
+        //labels
         fill(color);
         textAlign(CENTER);
+        noStroke();
         text(africanCountries[c], c*chartWidth, 10, chartWidth-10);
+        
+        //draw dividing lines
+        stroke(0, 0, 0);
+        line(c*chartWidth,100,c*chartWidth,windowHeight-100);
     }
+    
     
 }
 
@@ -34,7 +45,48 @@ function bars(x, y, val, color) {
             
 }
 
+function extrapolator(newYear, lastYear, x, y, val, color) {
+
+                //set lastYear to startYear if necessary
+                if (lastYear < startYear) {
+                    lastYear = startYear;
+                } 
+                
+                //find the number of years we need to infer data for
+                var yearDiff = newYear - lastYear;
+
+                //set y value and draw charts
+                for (var i = 1; i<yearDiff; i++) {
+                    y = ((lastYear+i-startYear) * 15) + 20;
+                    bars(x-val, y, val, color);
+                }
+                
+}
+
+function extrapolatorFade(newYear, lastYear, x, y, val, color) {
+    
+    // case that the last year read is before the years we're charting
+    if (lastYear < startYear) {
+        lastYear = startYear;
+    }
+    
+    //init y
+    y = ((lastYear-startYear) * 15) + 20;
+    
+    var yearDiff = (newYear - lastYear)*15;
+    
+    for (var i = 0; i<yearDiff; i++) {
+        
+        var opacity = map(i, yearDiff, 0, 0, 255);
+        stroke(70, 68, 67, opacity);
+        line(x-val, y+i, x, y+i);
+    }
+    
+}
+
 function showWIIDData(data) {
+    
+    var giniColor = color(70, 68, 67);
     
     var count = data.getRowCount();
     var rowHeight = (height/count);
@@ -50,8 +102,6 @@ function showWIIDData(data) {
     
     var val;
     
-    fill(212, 95, 95);
-    text(lastCountry, x, y-10);
     
     // start looping through rows
     for (var row=0; row<count; row++) {
@@ -72,42 +122,27 @@ function showWIIDData(data) {
             
             //extrapolate data for missing years
             if (newYear != lastYear && newYear != lastYear+1) {
-                
-                //set lastYear to startYear if necessary
-                if (lastYear < startYear) {
-                    lastYear = startYear;
-                } 
-                
-                //find the number of years we need to infer data for
-                var yearDiff = newYear - lastYear;
-                console.log(newCountry);
-                console.log('running YEARDIFF because newYear =' + newYear + 'and lastYear =' + lastYear);
-                
-                
-                //set y value and draw charts
-                for (var i = 1; i<yearDiff; i++) {
-                    y = ((lastYear+i-startYear) * 15) + 20;
-                    bars(x-val, y, val, 'pink');
-                    //console.log('running YEARDIFF=' + yearDiff + ' y= ' + y + ' x= ' + x);
-                }
-                
+                extrapolatorFade(newYear, lastYear, x, y, val, giniColor);
+            }
+            
+            /////////////this doesn't work yet  | extrapolate data for the last year
+            if (newCountry != africanCountries[africanCountries.length-1] && newCountry != data.getString(row+1, 0)) {
+                extrapolatorFade(endYear+1, lastYear, x, y, val, giniColor);
             }
             
             // value needs to be parameterized
             val = data.getString(row, 4);
             val = parseFloat(val);
-            val = map(val, 0, 100, 0, chartWidth/2);
+            val = map(val, 0, 100, 0, barWidth/2);
             
             //y is equal to the current year minus the start year
             y = ((newYear - startYear) * 15) + 20;
             
             // draw rectangle
-            bars(x-val, y, val, 'red');
-            
-            //text(newCountry + ' ' + x + ' ' + y, x, y);
-        
+            bars(x-val, y, val, giniColor);
         }
         
+        //update lastCountry / lastYear for conditionals
         lastCountry = newCountry;
         lastYear = newYear;
         
@@ -120,6 +155,9 @@ function showWIIDData(data) {
 }
 
 function showPTSData(data) {
+
+    var ptsColor = color(240, 97, 60);
+    
     var count = data.getRowCount();
     var rowHeight = (height/count);
     
@@ -129,7 +167,6 @@ function showPTSData(data) {
     var x = 0;
     var y = 20;
     
-    text(thisCountry, x, y-10);
     
     //start looping through rows
     for (var row=0; row<count; row++) {
@@ -145,10 +182,10 @@ function showPTSData(data) {
         
         val = data.getString(row, 5);
         val = parseFloat(val);
-        val = map(val, 0, 5, 0, chartWidth/2);
+        val = map(val, 0, 5, 0, barWidth/2);
         
         //hashLine(x, y, width, y);
-        fill(100,100,100);
+        fill(ptsColor, 100);
         noStroke();
         rect(x + leftPad, y, val, 15);
         
@@ -157,12 +194,4 @@ function showPTSData(data) {
         newCountry = thisCountry;
         
     }
-}
-
-function hashLine(x, y, width) {
-    
-    stroke(51,51,51);
-    line(x, y, width, y);
-    
-    
 }
